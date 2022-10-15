635cbdd017ecaaff9c2693fb390ce3b735379d88
안동민 <110483588+andongmin94@users.noreply.github.com>
2022-10-15 13:33:31 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,17 @@
+T = int(input())
+for tc in range(T):
+    str = input()
+    if str == 'Algorithm':
+        print(204)
+    if str == 'DataAnalysis':
+        print(207)
+    if str == 'ArtificialIntelligence':
+        print(302)
+    if str == 'CyberSecurity':
+        print('B101')
+    if str == 'Network':
+        print(303)
+    if str == 'Startup':
+        print(501)
+    if str == 'TestStrategy':
+        print(105)
