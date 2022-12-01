0fc45c3b67ebf82afda4dfc3db3ff1d4c1cedcaa
안동민 <110483588+andongmin94@users.noreply.github.com>
2022-12-01 21:20:11 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,11 @@
+T = input()
+if T == 'M':
+    print('MatKor')
+if T == 'W':
+    print('WiCys')
+if T == 'C':
+    print('CyKor')
+if T == 'A':
+    print('AlKor')
+if T == '$':
+    print('$clear')
