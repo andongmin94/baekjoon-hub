6f83715b401054205496ad1038fbe3a4f534bd81
안동민 <110483588+andongmin94@users.noreply.github.com>
2023-02-15 20:45:59 +0900

    [Bronze IV] Title: 移動 (Moving), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24079. 移動 （Moving）/移動 （Moving）.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x, y, z;
+    cin >> x >> y >> z;
+    if (x + y <= z + .5)
+         cout << 1;
+    else cout << 0;
+}
