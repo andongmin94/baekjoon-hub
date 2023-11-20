01c890b324e1b0eb7ee0abde7cf6c0779385ea19
안동민 <110483588+andongmin94@users.noreply.github.com>
2023-11-20 20:32:50 +0900

    [Bronze IV] Title: ISBN, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int a = 91;
+    int b, c, d;
+    cin >> b >> c >> d;
+    c = c * 3;
+    cout << "The 1-3-sum is " << a + b + c + d;
+}
