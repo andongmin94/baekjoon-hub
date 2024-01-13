1ebcf0c57934ff9775515f7e4d7faf28661734a8
안동민 <andongmin94@gmail.com>
2024-01-14 06:36:54 +0900

    [Bronze IV] Title: Gorivo, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14065. Gorivo/Gorivo.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long double a;
+    cin >> a;
+    cout << fixed << setprecision(6);
+    cout << 100 / 1.609344 / a * 3.785411784;
+}
