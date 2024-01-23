9be9ef23aaea3e13ff6f7195304313c0c7b4060a
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-01-23 11:18:07 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        cout << "Material Management " << i << "\nClassification ---- End!\n";
+    }
+}
