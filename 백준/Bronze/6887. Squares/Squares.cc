3508e607d6ed1c40971e349417e53a6320314e6e
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-01-28 10:15:16 +0900

    [Bronze IV] Title: Squares, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/6887. Squares/Squares.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int i;
+    cin >> i;
+    int j = sqrt(i);
+    cout << "The largest square has side length " << j << ".";
+}
