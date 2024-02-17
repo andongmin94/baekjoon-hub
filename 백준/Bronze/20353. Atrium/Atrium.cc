cc53927a5204e992452afe96984ee51236625666
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-02-17 12:35:37 +0900

    [Bronze IV] Title: Atrium, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long double a;
+    cin >> a;
+    cout << fixed << setprecision(8);
+    cout << sqrt(a) * 4;
+}
