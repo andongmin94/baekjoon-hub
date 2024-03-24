5c675e9aaac004a95e436464a35fbd753e277c7a
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-03-25 08:52:34 +0900

    [Bronze III] Title: 빙고 막기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32246. 빙고 막기/빙고 막기.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+
+    if (n != 2) cout << n;
+    else cout << 3;
+}
