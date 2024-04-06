b965acaf6f8d75045013b84981c1810e7e2b1a1a
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-04-06 12:08:00 +0900

    [Bronze IV] Title: 대각선, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34346. 대각선/대각선.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    if (n % 2) cout << 1;
+    else cout << 2;
+}
