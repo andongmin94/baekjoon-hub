c2a1e5548c0935b7f9d41b8c8d3101a888af7cbe
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-04-09 09:07:55 +0900

    [Bronze IV] Title: Простая задача, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30876. Tren del Fin del Mundo/Tren del Fin del Mundo.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    int min = 1000;
+    int min_l;
+
+    while(n--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (min >= b)
+        {
+            min_l = a;
+            min = b;
+        }
+    }
+
+    cout << min_l << ' ' << min;
+}
