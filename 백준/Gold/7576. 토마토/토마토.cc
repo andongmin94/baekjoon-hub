2b9616e30743bac9116f6e71fbb3013ff49ba7d3
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-04-11 05:02:18 +0900

    [Gold V] Title: 토마토, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -3,66 +3,66 @@
 using namespace std;
 
 int board[1000][1000];
-int vis[1000][1000] = { 0, };
+int visited[1000][1000];
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int dy[] = {0,1,0,-1};
+    int dx[] = {1,0,-1,0};
+
+    int m, n;
+    cin >> m >> n;
+
     queue<pair<int, int>> Q;
-    int max_v = 0;
-    bool minus_one = false;
-    bool zero = true;
 
-    int x, y;
-    cin >> y >> x;
-
-    for (int i = 0; i < x; i++)
-        for (int j = 0; j < y; j++)
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
         {
             cin >> board[i][j];
-            if (board[i][j] == 0)
-                zero = false;
             if (board[i][j] == 1)
             {
                 Q.push({i, j});
-                vis[i][j] = 1;
+                visited[i][j] = 1;
             }
-            if (board[i][j] == -1)
-                vis[i][j] = -1;
+            if (board[i][j] == -1) visited[i][j] = -1;
         }
+    }
 
-    int dx[4] = { 0,1,0,-1 };
-    int dy[4] = { 1,0,-1,0 };
-
-    while (!(Q.empty()))
+    while (!Q.empty())
     {
         pair<int, int> cur = Q.front();
         Q.pop();
-        for (int i = 0; i < 4; i++)
+
+        for(int i = 0; i < 4; i++)
         {
-            int nx = cur.first + dx[i];
-            int ny = cur.second + dy[i];
-            if (nx >= 0 && nx < x && ny >= 0 && ny < y && board[nx][ny] == 0 && vis[nx][ny] == 0)
+            int ny = cur.first + dy[i];
+            int nx = cur.second + dx[i];
+
+            if(ny < 0 || ny >= n) continue;
+            if(nx < 0 || nx >= m) continue;
+
+            if(board[ny][nx] == 0 && visited[ny][nx] == 0)
             {
-                Q.push({ nx, ny });
-                vis[nx][ny] = vis[cur.first][cur.second] + 1;
+                Q.push({ny, nx});
+                visited[ny][nx] = visited[cur.first][cur.second] + 1;
             }
         }
     }
 
-    for (int i = 0; i < x; i++)
-        for (int j = 0; j < y; j++)
+    int max = 0;
+    bool checker = false;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
         {
-            max_v = max(max_v, vis[i][j]);
-            if (vis[i][j] == 0)
-                minus_one = true;
+            if (visited[i][j] == 0) checker = true;
+            if (max < visited[i][j]) max = visited[i][j];
         }
-
-    if (zero == true)
-        cout << 0;
-    else if (minus_one)
-        cout << -1;
-    else cout << max_v - 1;
+    }
+    if (checker) cout << -1;
+    else cout << max - 1;
 }
