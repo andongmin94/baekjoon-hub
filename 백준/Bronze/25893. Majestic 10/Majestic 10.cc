db5f1928c6bb0ea2872cc67ebf54b0d4ad3bfa1e
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-04-13 21:24:49 +0900

    [Bronze IV] Title: Majestic 10, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25893. Majestic 10/Majestic 10.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    while(n--)
+    {
+        int a, cnt = 0, t = 3;
+        while(t--)
+        {
+            cin >> a;
+            cout << a << ' ';
+            if (t == 0) cout << '\n';
+            if (a >= 10) cnt++;
+        }
+
+        if (cnt == 0) cout << "zilch\n\n";
+        if (cnt == 1) cout << "double\n\n";
+        if (cnt == 2) cout << "double-double\n\n";
+        if (cnt == 3) cout << "triple-double\n\n";
+    }
+}
