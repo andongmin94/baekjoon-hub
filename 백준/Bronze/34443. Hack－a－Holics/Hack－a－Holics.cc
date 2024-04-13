8a57bac89d56f4360198bf92ca8957bbc95d876e
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-04-13 16:35:15 +0900

    [Bronze IV] Title: Hack-a-Holics, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34443. Hack－a－Holics/Hack－a－Holics.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, c, p; cin >> n >> c >> p;
+    cout << n * p;
+}
