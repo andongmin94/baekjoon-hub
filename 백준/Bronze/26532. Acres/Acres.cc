e4b282722f73c75faef80588b1f00dc3f664f07a
안동민 <andongmin94@gmail.com>
2024-04-15 09:57:40 +0900

    [Bronze IV] Title: Acres, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26532. Acres/Acres.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x, y;
+    cin >> x >> y;
+
+
+    cout << (x * y) / (4840 * 5) + 1;
+}
