eeec2c04a1477623ef4be61bbb052804b31de46f
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-04-22 09:25:41 +0900

    [Bronze IV] Title: Сравнение комнат, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27267. Сравнение комнат/Сравнение комнат.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+
+    if (a * b > c * d) cout << 'M';
+    else if (a * b < c * d) cout << 'P';
+    else cout << 'E';
+}
