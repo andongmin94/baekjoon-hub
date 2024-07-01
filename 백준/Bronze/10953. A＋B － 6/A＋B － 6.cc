7225e997ce78ad8fecfb1dc1b37d80d0c5e17faa
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-07-01 13:05:48 +0900

    [Bronze III] Title: A+B - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        int a, b;
+        char c;
+        cin >> a >> c >> b;
+        cout << a + b << '\n';
+    }
+}
