bab9ea55d60c541eea61de28286dc78b78f4e085
안동민 <andongmin94@gmail.com>
2024-07-02 13:25:12 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    cout << fixed << setprecision(6);
+    cout << pow(n, 2) * M_PI << '\n';
+    cout << 2 * pow(n, 2);
+    
+}
