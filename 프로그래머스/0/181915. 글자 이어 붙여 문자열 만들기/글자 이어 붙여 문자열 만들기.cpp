df2177dbb2fd45ba3f74344ccf24407f98d88772
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-07-04 20:02:15 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string my_string, vector<int> index_list) {
+    string answer = "";
+    //   
+    for (const auto& e : index_list)
+        answer += my_string[e];
+    //
+    return answer;
+}
