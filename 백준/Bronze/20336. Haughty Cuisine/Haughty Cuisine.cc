856059a6b9146acc95aaf6c939b2dacf1d99e638
안동민 <andongmin94@gmail.com>
2024-07-13 07:31:57 +0900

    [Bronze IV] Title: Haughty Cuisine, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/20336. Haughty Cuisine/Haughty Cuisine.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    while(n--)
+    {
+        int a;
+        cin >> a;
+        cout << a << '\n';
+        while(a--)
+        {
+            string s;
+            cin >> s;
+            cout << s << '\n';
+        }
+    }
+}
