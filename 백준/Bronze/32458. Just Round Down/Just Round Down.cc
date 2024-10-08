fed8ac7946a10ec1dfa91e7cc86263fc0d533bb6
안동민 <andongmin94@gmail.com>
2024-10-09 00:39:31 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    double n;
+    cin >> n;
+    cout << static_cast<int>(n);
+}
