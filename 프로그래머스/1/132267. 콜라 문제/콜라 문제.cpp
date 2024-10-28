9b6b7ae4ecf0aa7d5871c64a25a1e87b1b0d09a1
안동민 <andongmin94@gmail.com>
2024-10-29 08:23:27 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    //
+    while(n / a > 0)
+    {
+        answer += b * (n / a);
+        n = n % a + b * (n / a);
+    }
+    //
+    return answer;
+}
