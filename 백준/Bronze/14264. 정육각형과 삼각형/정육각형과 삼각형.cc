33ac474a53fab1cd98b810d07adc80a8a05e3fcb
안동민 <andongmin94@gmail.com>
2024-11-23 00:23:14 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.08 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 백준/Bronze/14264. 정육각형과 삼각형/정육각형과 삼각형.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long double l;
+    cin >> l;
+
+    cout << fixed << setprecision(14) << l * l * sqrt(3) / 4;
+}
