b7747988f1d7a76a3af7aba2520f106b75777eda
안동민 <andongmin94@gmail.com>
2024-11-28 00:35:41 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.cpp
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.cpp
@@ -4,9 +4,5 @@
 using namespace std;
 
 int solution(int num1, int num2) {
-    int answer = 0;
-    //
-    answer = num1 * num2; 
-    //
-    return answer;
+    return [&]() -> int { return num1 * num2; }();
 }
