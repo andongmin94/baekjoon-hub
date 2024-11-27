a83aca3948baf3e711de387f98c6bae9fcb977cd
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-11-28 00:35:41 +0900

    [Bronze IV] Title: 구구단표, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.cpp
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.cpp
@@ -4,9 +4,5 @@
 using namespace std;
 
 int solution(int num1, int num2) {
-    int answer = -1;
-    //
-    answer = num1 % num2;
-    //
-    return answer;
+    return [&]{return num1 % num2;}();
 }
