f0409afd528f09ef3fcfee4fc6cd4194542156df
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-12-07 12:16:30 +0900

    [Bronze III] Title: 수찬은 마린보이야!!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15921. 수찬은 마린보이야！！/수찬은 마린보이야！！.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    if (n != 0) cout << fixed << setprecision(2) << 1.00;
+    else cout << "divide by zero";
+}
