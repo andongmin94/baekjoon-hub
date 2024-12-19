aec8d6d4e6b0430cee64ac0c8963530ef58d8938
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-12-19 19:24:23 +0900

    [Bronze III] Title: 행운의 편지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.cpp
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.cpp
@@ -1,8 +1,8 @@
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int solution(int num1, int num2) {
-    return [&]{ return num1 - num2; }();
+    function<int()> a = [&]() -> int { return num1 - num2; };
+    return a();
 }
