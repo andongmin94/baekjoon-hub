e9f2e622f9616ebf169630236566751474203bf4
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-12-27 12:00:44 +0900

    [Bronze III] Title: 줄 세기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4806. 줄 세기/줄 세기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int cnt = 0;
+    string s;
+    while(getline(cin, s))
+    {
+        cnt++;
+    }
+    cout << cnt;
+}
