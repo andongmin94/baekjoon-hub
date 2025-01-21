ec72eb248793faccb2560e1b599fed2969768e6b
안동민 <andongmin94@gmail.com>
2025-01-21 09:55:58 +0900

    [Bronze III] Title: 스트릿 코딩 파이터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 250120/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        System.out.println("9.2ft = 280.4cm");
+        System.out.print("1.3mi = 209214.2cm");
+    }
+}
