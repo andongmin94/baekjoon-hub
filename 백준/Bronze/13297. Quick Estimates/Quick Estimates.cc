bf3443300e85291595784b31c8c1b23260a90cd7
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-03-10 11:01:47 +0900

    [Bronze IV] Title: Quick Estimates, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+    while (n--)
+    {
+        string s; cin >> s;
+        cout << s.length() << '\n';
+    }
+}
