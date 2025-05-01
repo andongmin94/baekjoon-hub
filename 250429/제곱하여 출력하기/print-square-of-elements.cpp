b585b2b992858af53397ceee0cf03ab1c2641339
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-05-02 08:19:56 +0900

    [Bronze III] Title: Collatz Conjecture, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 250429/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n; cin >> n;
+    while(n--)
+    {
+        int a; cin >> a;
+        cout << (int)(a * a) << ' ';
+    }
+    return 0;
+}
