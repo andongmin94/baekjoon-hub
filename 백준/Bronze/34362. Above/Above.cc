58243a82021b1d9fb1a3e7d99a68b93e75c1a7ff
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-09-20 08:17:43 +0900

    [Bronze V] Title: Above, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34362. Above/Above.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    double n; cin >> n;
+    cout << fixed << setprecision(4) << n - 0.3;
+}
