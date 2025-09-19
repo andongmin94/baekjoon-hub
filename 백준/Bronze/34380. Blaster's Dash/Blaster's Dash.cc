c65e535aaf1423f5e2b5fc8ee55d9b31ccb15090
안동민 <andongmin94@gmail.com>
2025-09-20 08:17:43 +0900

    [Bronze V] Title: Blaster's Dash, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34380. Blaster's Dash/Blaster's Dash.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n >> n >> n;
+    cout << (n + 20) * 2;
+    cin >> n;
+}
