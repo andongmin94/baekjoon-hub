468d8a12a4bff89a1bb12e9541c9433ec974447a
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-09-24 18:40:34 +0900

    [Bronze V] Title: Roller Coaster Ride, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34454. Roller Coaster Ride/Roller Coaster Ride.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b, c; cin >> a >> b >> c;
+    if (b * c >= a) cout << "yes";
+    else cout << "no";
+}
