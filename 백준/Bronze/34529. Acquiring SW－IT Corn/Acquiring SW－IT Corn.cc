89b6eab41a986ad25cb2233b8eece008c0a8f796
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-09-28 10:44:19 +0900

    [Bronze IV] Title: Acquiring SW-IT Corn, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34529. Acquiring SW－IT Corn/Acquiring SW－IT Corn.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x,y,z,u,v,w; cin>>x>>y>>z>>u>>v>>w;
+    cout <<
+        u / 100 * x
+      + v / 50 * y 
+      + w / 20 * z;
+}
