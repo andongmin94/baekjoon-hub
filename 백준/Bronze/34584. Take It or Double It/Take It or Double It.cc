4044010fb083ec29c6064faed5602c24ed0c7309
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-10-05 13:25:47 +0900

    [Bronze IV] Title: Take It or Double It, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34584. Take It or Double It/Take It or Double It.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x, d; cin >> x >> d;
+    if (x * 2 > d) cout << "take it";
+    else cout << "double it";
+}
