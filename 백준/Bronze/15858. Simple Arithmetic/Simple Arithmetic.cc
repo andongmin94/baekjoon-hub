322a50efc74e642445610e35bf59242299d2b764
안동민 <andongmin94@gmail.com>
2025-10-25 12:56:22 +0900

    [Bronze III] Title: Perfect Cube, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15858. Simple Arithmetic/Simple Arithmetic.cc
+++ 백준/Bronze/15858. Simple Arithmetic/Simple Arithmetic.cc
@@ -7,7 +7,13 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    double a, b, c; cin >> a >> b >> c;
-    cout << fixed << setprecision(16);
-    cout << a * b / c;
+    long long a, b, c, d;
+    cin >> a >> b >> c;
+    d = a * b;
+    if (d >= 10 * c)
+    {
+        cout << d / (10 * c);
+        d %= 10 * c;
+    }
+    cout << setprecision(18) << fixed << (long double)d / c;
 }
