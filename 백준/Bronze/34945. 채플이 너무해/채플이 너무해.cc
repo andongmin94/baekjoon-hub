6a33e0d38e4a05ed81fa4ee177e3c031395a52b0
안동민 <andongmin94@gmail.com>
2025-12-24 08:06:43 +0900

    [Bronze V] Title: 채플이 너무해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n; cin >> n;
+    if (n >= 6) cout << "Success!";
+    else cout << "Oh My God!";
+}
