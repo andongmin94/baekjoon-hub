ba40b6da560f951200fdd5ae02940e27b68a49b6
안동민 <110483588+andongmin94@users.noreply.github.com>
2026-01-01 11:26:57 +0900

    [level 0] Title: 세균 증식, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int t) {
+        int answer = n;
+        while(t > 0) {
+            t--;
+            answer *= 2;
+        }
+        return answer;
+    }
+}
