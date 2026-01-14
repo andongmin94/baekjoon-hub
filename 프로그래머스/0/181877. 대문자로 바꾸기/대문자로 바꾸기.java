dcf190ed334389aaeafd6ca6bca5e336a050b3f7
안동민 <110483588+andongmin94@users.noreply.github.com>
2026-01-14 18:41:08 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 6.74 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for (int i = 0; i < myString.length(); i++) {
+            answer += Character.toUpperCase(myString.charAt(i));
+        }
+        return answer;
+    }
+}
