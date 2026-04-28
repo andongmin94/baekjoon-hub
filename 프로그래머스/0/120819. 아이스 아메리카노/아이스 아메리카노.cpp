bd57c351be7147be3581172b9f09d6edb04c8d3f
안동민 <110483588+andongmin94@users.noreply.github.com>
2026-04-29 08:14:11 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.cpp
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 
 vector<int> solution(int money) {
-    vector<int> answer;
-    //
-    answer.push_back(money / 5500);
-    answer.push_back(money % 5500);
-    //
+    vector<int> answer = [&]() -> vector<int>
+    {
+        vector<int> v;
+        v.push_back(money / 5500);
+        v.push_back(money % 5500);
+        return v;
+    }();
     return answer;
 }
