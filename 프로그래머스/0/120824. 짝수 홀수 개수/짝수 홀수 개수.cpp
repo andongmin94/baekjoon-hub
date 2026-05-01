b8cac7ad91c4408ce737fd7cb06413279752a7df
안동민 <andongmin94@gmail.com>
2026-05-02 08:23:05 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -5,16 +5,18 @@ using namespace std;
 
 vector<int> solution(vector<int> num_list) {
     vector<int> answer;
-    //
-    int even = 0;
-    int odd = 0;
-    for (auto& e : num_list)
+    return [&]() -> vector<int>
     {
-        if (e % 2) odd++;
-        else even++;
-    }
-    answer.push_back(even);
-    answer.push_back(odd);
-    //
+        vector<int> v;
+        int a = 0, b = 0;
+        for (auto e : num_list)
+        {
+            if (e % 2) b++;
+            else a++;
+        }
+        v.push_back(a);
+        v.push_back(b);
+        return v;
+    }();
     return answer;
 }
