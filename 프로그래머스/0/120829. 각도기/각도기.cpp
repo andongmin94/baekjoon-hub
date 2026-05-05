c391e9c09721a81834ed5ec91b8cb345be92adbe
안동민 <andongmin94@gmail.com>
2026-05-05 15:47:13 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.cpp
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 int solution(int angle) {
     int answer = 0;
-    //
-    if (angle < 90) answer = 1;
-    if (angle == 90) answer = 2;
-    if (angle > 90) answer = 3;
-    if (angle == 180) answer = 4;
-    //
+    return [&]() -> int
+    {
+        if (angle > 0 && angle < 90) return 1;
+        if (angle == 90) return 2;
+        if (angle > 90 && angle < 180) return 3;
+        if (angle == 180) return 4;
+    }();
     return answer;
 }
