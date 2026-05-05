197560927869085c36ca93e142b15e93181faade
안동민 <andongmin94@gmail.com>
2026-05-06 00:54:17 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.cpp
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cpp
@@ -5,8 +5,12 @@ using namespace std;
 
 int solution(int n, int k) {
     int answer = 0;
-    //
-    answer = n * 12000 + (k - n / 10) * 2000 ;
-    //
+    return [&]() -> int
+    {
+        answer += n * 12000;
+        answer += k * 2000;
+        answer -= n / 10 * 2000;
+        return answer;
+    }();
     return answer;
 }
