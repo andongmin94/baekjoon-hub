6a359d582359ef1157cb0681d42eed4fac1f7cbc
안동민 <110483588+andongmin94@users.noreply.github.com>
2026-05-13 01:12:42 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 3.82 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -1,15 +1,20 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int solution(string my_string) {
     int answer = 0;
-    //
-    for (auto& e : my_string)
+    return [&]() -> int
     {
-        if ('0' <= e && e <= '9')
-            answer += static_cast<int>(e - '0');
-    }
-    //
+        for (auto e : my_string)
+        {
+            if (isdigit(e))
+            {
+                answer += e - '0';
+            }
+        }
+        return answer;
+    }();
     return answer;
 }
