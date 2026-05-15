dd61927dd7b4259a61a742a9e6344536760941c4
안동민 <110483588+andongmin94@users.noreply.github.com>
2026-05-15 11:06:53 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = start_num; i <= end_num; i++) {
+            arr.add(i);
+        }
+        int[] answer = new int[arr.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = arr.get(i);
+        }
+        return answer;
+    }
+}
