1b546df475d757c214fd1e3fe688252e5fe3f321
안동민 <110483588+andongmin94@users.noreply.github.com>
2026-06-21 03:15:11 +0900

    [Passed] 합과 평균 (139ms, 11MB)

---
+++ 코드트리/trail0/합과 평균/sum-and-avg.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        double c = (a + b) / 2.0;
+        System.out.print((a+b) + " " + c);
+    }
+}
